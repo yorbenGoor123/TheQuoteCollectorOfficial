59e151eff0c137657b2cdfc3ed33b4fe4bcb944a
yorbenGoor123 <yorbengoor5@gmail.com>
2019-10-23 16:00:21 +0200

    add overview

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ $button.addEventListener('click',handleClick);
 }
 
 const handleClick = (e) => {
-  console.log(e.Prevendefault);
+  const $text = document.querySelector(".input").value;
+  const $table = document.querySelector(".overview__table");
+  const today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //As January is 0.
+  var yyyy = today.getFullYear();
+  $table.innerHTML += `<tr class="quote__example">
+  <td class="quote">${$text}</td>
+  <td class="date">${dd}/${mm}/${yyyy}</td>
+</tr>`;
+  console.log($text);
+
+
 }
 init();
 }
